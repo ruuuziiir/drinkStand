244e8d658171c91985cae37b9105d8f2058b9f60
7 <ruuuziiir@gmail.com>
2020-12-15 18:26:26 +0800

    Initial Commit

---
+++ drinks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  drinks
+//
+//  Created by 7 on 2020/12/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
